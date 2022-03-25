9539f9332993f681a5395958c3161f3c57a4f303
Yulya163 <suzdlik@gmail.com>
2022-03-25 13:49:05 +0400

    some links added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,8 +2,13 @@ function Footer() {
     return <footer className="page-footer #b2dfdb teal lighten-4">        
         <div className="footer-copyright">
             <div className="container">
-            © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+            © {new Date().getFullYear()} Copyright Text           
+            <a 
+                className="grey-text text-lighten-4 right"
+                href="https://yulya163.github.io/movies-project/"
+                target="blank" 
+                rel="noreferrer"
+            >Repo</a>
             </div>
         </div>
     </footer>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,15 @@
 function Header() {
     return <nav className="#26a69a teal lighten-1">
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Movies</a>
+      <a href="/" className="brand-logo">React Movies</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#">Repo</a></li>        
+        <li>
+          <a 
+            href="https://yulya163.github.io/movies-project/"
+            target="blank" 
+            rel="noreferrer"
+          >Repo</a>
+        </li>        
       </ul>
     </div>
   </nav>
